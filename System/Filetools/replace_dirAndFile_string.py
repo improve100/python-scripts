5c87559c9935ee27d8a9f887259ae8de3fc246e2
tongchangjin <939896399@qq.com>
2018-02-01 17:43:17 +0800

    update replaceDirAndFileString.py

--- System/Filetools/replace_dirAndFile_string.py
+++ System/Filetools/replace_dirAndFile_string.py
@@ -1,38 +1,54 @@
+#!/usr/bin/env python
 
 import sys, os
 
-oldstr = "kobuki"
+oldstr = "yujin"
 newstr = "vking"
-
+Oldstr = "Yujin"
+Newstr = "Vking"
+OLDSTR = "YUJIN"
+NEWSTR = "VKING" 
 
 def lister(root):                                           # for a root dir
-    for (thisdir, subshere, fileshere) in os.walk(root):    # generate dirs in tree
+    if not os.path.isdir(root):
+        print(root,"is not directory!")
+        sys.exit(-1)
+#    print("test")
+#    sys.exit(0)
+    if oldstr in root:
+        newroot = root.replace(oldstr,newstr)
+        os.rename(root,newroot) 
+    else:
+        newroot = root
+    for (thisdir, subshere, fileshere) in os.walk(newroot):    
         if "/." in thisdir:
             continue
         print('[' + thisdir + ']')
-        if 'kobuki' in thisdir:
-            newthisdir = thisdir.replace('kobuki','vking')
+        if oldstr in thisdir:
+            newthisdir = thisdir.replace(oldstr,newstr)
             os.rename(thisdir,newthisdir) 
             print('[new' + newthisdir + ']')
-            thisdir = newthisdir
-        for fname in fileshere:                             # print files in this dir
+            thisdir = newthisdir    #have a bug: if rename the directory,os.walk cann't execute full,must run more times
+            #lister(thisdir)
+        for fname in fileshere:                             
             if fname.startswith('.'):
                 continue
-            path = os.path.join(thisdir, fname)             # add dir name prefix
+            path = os.path.join(thisdir, fname)                                  
             print('****'+path)
-            if "kobuki" in fname:
-                newpath = path.replace('kobuki','vking')
+            if oldstr in fname:
+                newpath = path.replace(oldstr,newstr)
                 os.rename(path,newpath)
                 print('&&&&'+newpath)
                 path = newpath
             if os.path.exists(path):
                 with open(path,'r+') as f:
                     t = f.read()
-                    t = t.replace('kobuki', 'vking')
-                    t = t.replace('Kobuki', 'Vking')
-                    t = t.replace('KOBUKI', 'VKING')
+                    t = t.replace(oldstr,newstr)
+                    t = t.replace(Oldstr,Newstr)
+                    t = t.replace(OLDSTR,NEWSTR)
                     f.seek(0, 0)    
+                    f.truncate()
                     f.write(t)
 
 if __name__ == '__main__':
-    lister(sys.argv[1])                                     # dir name in cmdline
+    lister(sys.argv[1])                                                          
